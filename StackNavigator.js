69760d79dd21d87a114bdf2670bccfe14e0e81b8
Etienne de Dianous <etiennededi@hotmail.fr>
2022-09-21 12:26:59 +0200

    ok

--- StackNavigator.js
+++ StackNavigator.js
@@ -6,16 +6,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HoroscopeScreen from './screens/HoroscopeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import LoginScreen from './screens/LoginScreen';
+import useAuth from './hooks/useAuth';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
+  const { user } = useAuth();
   return (
   <Stack.Navigator>
+    {user ? (
+    <>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Matches" component={MatchesScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Horoscope" component={HoroscopeScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
+    </>
+    ) : (
     <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
-    <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-    <Stack.Screen name="Matches" component={MatchesScreen} options={{ headerShown: false }}/>
-    <Stack.Screen name="Horoscope" component={HoroscopeScreen} options={{ headerShown: false }}/>
-    <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
+    )}
   </Stack.Navigator>
   )
 }
